66814513553fc1b2f78d56ae20494d7a44659206
Aravind Rajesh Kanna <22373811+AravindRajeshkanna@users.noreply.github.com>
2017-09-22 22:11:04 +0530

    Code refactoring

--- webCrawler.js
+++ webCrawler.js
@@ -1,62 +1,18 @@
 //Graph Data Structure
-function Graph() {
-  this.vertices = [
-    'http://www.amazon.com',
-    'https://stackoverflow.com', 
-    'https://en.wikipedia.org/wiki/Main_Page', 
-    'https://twitter.com/?lang=en',
-    'https://www.microsoft.com/en-in'
-  ];
-  this.edges = [];
-  this.edges['http://www.amazon.com'] = [];
-  this.edges['https://stackoverflow.com'] = [];
-  this.edges['https://en.wikipedia.org/wiki/Main_Page'] = [];
-  this.edges['https://twitter.com/?lang=en'] = [];
-  this.edges['https://www.microsoft.com/en-in'] = [];
-  this.numberOfEdges = 0;
-}
-
-Graph.prototype.getVertices = function() {
-  return this.vertices;
-};
-
-Graph.prototype.getVertex = function(index) {
-  return this.vertices[index];
-}
-
-Graph.prototype.addVertex = function(vertex) {
-  this.vertices.push(vertex);
-  this.edges[vertex] = [];
-};
-
-Graph.prototype.addEdge = function(vertex1, vertex2) {
-  this.edges[vertex1].push(vertex2);
-  this.numberOfEdges++;
-};
-
-Graph.prototype.size = function() {
-  return this.vertices.length;
-};
-
-Graph.prototype.print = function() {
-  console.log(this.vertices.map(function(vertex) {
-    return (vertex + ' -> ' + this.edges[vertex].join(', ')).trim();
-  }, this).join(' | '));
-};
-
-const graph = new Graph();
+const Graph = require('./graph');
+let graph = new Graph();
 
 var index = 0;
 
 //Crawler
 const Crawler = require("crawler");
-const c = new Crawler({
+const crawler = new Crawler({
     maxConnections : 1,
     // This will be called for each crawled page
     callback : function (error, res, done) {
-      if(error){
+      if(error) {
           console.log(error);
-      } else{
+      } else {
           var $ = res.$;
           // Won't allow resource link
           if (typeof $ === "function") {
@@ -68,7 +24,7 @@ const c = new Crawler({
                 // TODO: Check if link already present or not
                 graph.addVertex(link);
                 graph.addEdge(branchURL, link);
-                c.queue(link);
+                crawler.queue(link);
               });
               console.log(graph.size());
           }
@@ -77,4 +33,4 @@ const c = new Crawler({
       index++;
     }
 });
-c.queue(graph.getVertices());
+crawler.queue(graph.getVertices());
