7d055bd17e4847b0db4c6c3dd0054e3a35ba5826
aravind <arvindkousalya@gmail.com>
2017-09-22 23:16:08 +0530

    Getting seed URL from JSON file

--- graph.js
+++ graph.js
@@ -1,19 +1,8 @@
 class Graph {
-
+    
 	constructor() {
-		this.vertices = [
-		  'http://www.amazon.com',
-		  'https://stackoverflow.com', 
-		  'https://en.wikipedia.org/wiki/Main_Page', 
-		  'https://twitter.com/?lang=en',
-		  'https://www.microsoft.com/en-in'
-		];
+        this.vertices = [];
 		this.edges = [];
-		this.edges['http://www.amazon.com'] = [];
-		this.edges['https://stackoverflow.com'] = [];
-		this.edges['https://en.wikipedia.org/wiki/Main_Page'] = [];
-		this.edges['https://twitter.com/?lang=en'] = [];
-		this.edges['https://www.microsoft.com/en-in'] = [];
 		this.numberOfEdges = 0;
 	}
 
