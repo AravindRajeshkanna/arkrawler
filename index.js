43992fa93c57e4f4e015aaa84d7685613a97a74f
Aravind Rajesh Kanna <22373811+AravindRajeshkanna@users.noreply.github.com>
2017-09-24 10:39:35 +0530

    Garbage collection statistics and Page count logging

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ const redis = require('redis');
 const redisClient = redis.createClient();
 
 // Process
-// TODO: Need to handle heap memory size exceed issue
+// TODO: Need to handle heap memory size exceed issue and resume the process
 const process = require('process');
 if (process.pid) {
   console.log('This process is your pid ' + process.pid);
 }
 
+// Garbage collection statistics
+const gc = (require('gc-stats'))();
+gc.on('stats', function (stats) {
+    console.log('Used heap size:' + stats.after.usedHeapSize);
+});
+
+// Crawled page count
+var crawledPageCount = 0;
+
 // Crawler
 const Crawler = require("crawler");
 const crawler = new Crawler({
-    maxConnections: 10,
+    maxConnections: 100,
     // This will be called for each crawled page
     callback: function (error, res, done) {
         if (error) {
-            console.log(error);
+            console.error(error);
         } else {
             var $ = res.$;
+            crawledPageCount++;
+            console.log('page count:' + crawledPageCount);
             // Won't allow resource link
             if (typeof $ === "function") {
                 // $ is Cheerio by default, a lean implementation of core jQuery designed specifically for the server
@@ -29,7 +40,7 @@ const crawler = new Crawler({
                 });
                 crawler.queue(absoluteLinks);
                 redisClient.sadd('seeds', absoluteLinks, function(err, reply) {
-                    console.log(reply);
+                    console.log('seeds:' + reply);
                 });
             }
         }
@@ -41,12 +52,12 @@ const crawler = new Crawler({
 const fs = require('fs');
 fs.readFile('./seed.json', 'utf8', function (err, data) {
     if (err) {
-        return console.log(err);
+        return console.error(err);
     }
     const urls = JSON.parse(data);
     crawler.queue(urls);
     redisClient.sadd(urls);
     redisClient.sadd('seeds', urls, function(err, reply) {
-        console.log(reply);
+        console.log('redis init:' + reply);
     });
 });
