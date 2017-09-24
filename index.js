31e35446d0b74a892731bdeff9499de5c82e0ffc
Aravind Rajesh Kanna <22373811+AravindRajeshkanna@users.noreply.github.com>
2017-09-25 00:23:29 +0530

    Resursive child process

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const process = require('process');
-const exec = require('child_process').exec;
+const childProcess = require('child_process');
 
 // Process
 if (process.pid) {
   console.log('pid:' + process.pid);
 }
 
-// TODO: Need to start child process after it is stopped
-exec(`cd ${__dirname} && node crawler.js`, function(error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});
+// TODO: Need to resume from last crawled url and need to handle initial crawler queueing separately
+respawn = () => {
+    var crawler = childProcess.exec(`cd ${__dirname} && node crawler.js`, function(error, stdout, stderr) {
+        // TODO: Need to store it in file
+        // console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
+    crawler.on('exit', function (code, signal) {
+        console.log('exit code: ', code);
+        console.log('exit signal: ', signal);
+        console.warn('\nCNTL-C to exit.\n');
+        crawler = respawn();
+    });
+}
+respawn();
