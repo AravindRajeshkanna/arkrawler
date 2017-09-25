1ca5d2f5ae76261e826ce5e1e948a6537c64d287
aravind <arvindkousalya@gmail.com>
2017-09-25 22:49:24 +0530

    Exiting child process

--- crawler.js
+++ crawler.js
@@ -1,42 +1,47 @@
 const crawler = require("crawler");
 const redis = require('redis');
+const process = require('process');
 
 // Redis
 const redisClient = redis.createClient();
 
-// Crawled page count
+// Crawled page count for single child process
 var crawledPageCount = 0;
 
 // Crawler
 const crawlerInstance = new crawler({
     maxConnections: 100,
     // This will be called for each crawled page
-    callback: function (error, res, done) {
+    callback: function(error, res, done) {
         if (error) {
             console.error(error);
         } else {
             var $ = res.$;
             crawledPageCount++;
-            console.log('page count:' + crawledPageCount);
-            redisClient.hset("crawler", "pageCount", crawledPageCount);
+            console.log(`page count:${crawledPageCount}`);
             // Won't allow resource link
             if (typeof $ === "function") {
                 // $ is Cheerio by default, a lean implementation of core jQuery designed specifically for the server
                 var absoluteLinks = [];
-                $("a[href^='http']").each(function () {
+                $("a[href^='http']").each(function() {
                     var link = $(this).attr('href');
                     absoluteLinks.push(link);
                 });
-                redisClient.sadd('seeds', absoluteLinks, function(err, reply) {
-                    // TODO: Need to find undefined scenario
-                    console.log('seeds:' + reply);
-                });
+                // TODO: Need to find undefined scenario
+                if(absoluteLinks.length) {
+                    redisClient.sadd('seeds', absoluteLinks, function(err, reply) {
+                        console.log(`seeds:${reply}`);
+                    });
+                }
             }
         }
         done();
+        if(crawledPageCount === 1000) {
+            process.exit();
+        }
     }
 });
 
-redisClient.srandmember('seeds', 10000, function(err, reply) {
+redisClient.srandmember('seeds', 1000, function(err, reply) {
     crawlerInstance.queue(reply);
 });
